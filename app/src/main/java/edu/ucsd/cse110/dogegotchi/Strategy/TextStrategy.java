06bbd0a5af114e3df4405a49b7e8ab10a3b92829
ngoduy1001 <dkn019@ucsd.edu>
2019-03-07 11:57:35 -0800

    hello

--- app/src/main/java/edu/ucsd/cse110/dogegotchi/Strategy/TextStrategy.java
+++ app/src/main/java/edu/ucsd/cse110/dogegotchi/Strategy/TextStrategy.java
@@ -20,11 +20,11 @@ public class TextStrategy implements Strategy {
     public void drawToCanvas(Canvas canvas){
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
-        //paint.setStyle(Paint.Style.FILL);
-        //canvas.drawRect(this.textCoord.getX() - 200, this.textCoord.getY() + 200, this.textCoord.getX() + 200, this.textCoord.getY() - 200, paint);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(this.textCoord.getX() - 200, this.textCoord.getY() + 200, this.textCoord.getX() + 200, this.textCoord.getY() - 200, paint);
         paint.setColor(Color.BLACK);
         paint.setTextSize(30);
-        canvas.drawText(this.text, this.textCoord.getX() + 20, this.textCoord.getY(), paint);
+        canvas.drawText(this.text, this.textCoord.getX() + 20, this.textCoord.getY() - 20, paint);
     }
 }
 
